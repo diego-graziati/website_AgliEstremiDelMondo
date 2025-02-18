44938c9b3720e5850bc56dfe949fdae0dbda4c0d
Diego Graziati <graziati@antek.it>
2025-02-18 13:50:02 +0100

    Debugging and fixing routing issues

--- sources/scripts/header.php
+++ sources/scripts/header.php
@@ -16,10 +16,17 @@ $request_path = parse_url($request_uri, PHP_URL_PATH);
 $routing_info = $router->getCompleteRoutingInfo($request_path);
 error_log("[".date("Y-M-D h:m:s")."] [Info] [header.php] Request URI: " . print_r($request_uri, true) ."\n", 3, PHP_LOGS_FILE_PATH);
 $request_post_body = "";
+$internal_request = false;
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $body = file_get_contents("php://inputs");
+    $body = file_get_contents("php://input");
     $request_post_body = json_decode($body, true);
     error_log("[".date("Y-M-D h:m:s")."] [Info] [header.php] POST request JSON body: " . print_r($request_post_body, true) ."\n", 3, PHP_LOGS_FILE_PATH);
+
+    $internal_request = isset($request_post_body['internal-request']) ? $request_post_body['internal-request']: false;
 }
-$router->callRoute($routing_info, $request_post_body);
+
+error_log("[".date("Y-M-D h:m:s")."] [Info] [header.php] Internal request: " . (($internal_request) ? "true" : "false") ."\n", 3, PHP_LOGS_FILE_PATH);
+if(!$internal_request){
+    $router->callRoute($routing_info, $request_post_body);
+}
 ?>
--- sources/scripts/php/routing.php
+++ sources/scripts/php/routing.php
@@ -24,7 +24,7 @@
                 $file_content = file_get_contents($file_path);
                 $routes_data = json_decode($file_content, true);
                 if (isset($routes_data["routes"])) {
-                    $this->processRoutes($routes_data["routes"], "", 1,MAX_ROUTING_DEPTH);
+                    $this->processRoutes($routes_data["routes"], "", 1, MAX_ROUTING_DEPTH);
                 }
                 if (isset($routes_data["fallback_route"])) {
                     error_log("[".date("Y-M-D h:m:s")."] [Info] [routing.php -> __construct] Fallback route set\n" , 3, PHP_LOGS_FILE_PATH);
@@ -32,8 +32,6 @@
                     if(!file_exists(ROOT_DIR_PATH . "/" . $this->fallback_route['php_file_path'])){
                         error_log("[".date("Y-M-D h:m:s")."] [Error] [routing.php -> __construct function] Php file path ".ROOT_DIR_PATH."/".$this->fallback_route['php_file_path']." doesn't exist\n" , 3, PHP_LOGS_FILE_PATH);
                         die();
-                    }else{
-                        error_log("[".date("Y-M-D h:m:s")."] [Info] [routing.php -> __construct function] Php file path ".ROOT_DIR_PATH."/".$this->fallback_route['php_file_path']." exists\n" , 3, PHP_LOGS_FILE_PATH);
                     }
                     $this->fallback_route['actual_php_file_path'] = PROTOCOL . HOST . "/" . $this->fallback_route['php_file_path'];
                 }
@@ -52,15 +50,13 @@
                 if(!file_exists(ROOT_DIR_PATH . "/" . $route_copy['php_file_path'])){
                     error_log("[".date("Y-M-D h:m:s")."] [Error] [routing.php -> processRoutes function] Php file path ".ROOT_DIR_PATH."/".$route_copy['php_file_path']." doesn't exist\n" , 3, PHP_LOGS_FILE_PATH);
                     die();
-                }else{
-                    error_log("[".date("Y-M-D h:m:s")."] [Info] [routing.php -> processRoutes function] Php file path ".ROOT_DIR_PATH."/".$route_copy['php_file_path']." exists\n" , 3, PHP_LOGS_FILE_PATH);
                 }
                 $route_copy['actual_php_file_path'] = PROTOCOL . HOST . "/" . $route_copy['php_file_path'];
                 unset($route_copy['children']);
 
                 // Aggiungi la route all'array associativo.
                 $this->routes[$current_path] = $route_copy;
-                error_log("[".date("Y-M-D h:m:s")."] [Info] [routing.php -> processRoutes function] New route set: ".$current_path."\n" , 3, PHP_LOGS_FILE_PATH);
+                #error_log("[".date("Y-M-D h:m:s")."] [Info] [routing.php -> processRoutes function] New route set: ".$current_path."\n" , 3, PHP_LOGS_FILE_PATH);
 
                 // Se ci sono figli e non si supera la profondità massima, processali ricorsivamente.
                 if ($depth < $max_depth && !empty($route['children'])) {
@@ -99,13 +95,20 @@
                 }
             }
 
+            // Per evitare loop infiniti e capire chi stia esattamente compiendo la richiesta, inseriamo il seguente flag
+            $post_parameters['internal_request'] = true;
+
             // Inizializza cURL
             $ch = curl_init();
 
             // Configura cURL
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_parameters));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen(json_encode($post_parameters))
+            ]);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_parameters));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Per ricevere la risposta
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Segue automaticamente il redirect
             curl_setopt($ch, CURLOPT_TIMEOUT, 5);
